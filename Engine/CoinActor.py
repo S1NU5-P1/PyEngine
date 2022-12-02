0dc087c25a1c1a8bd90cdfd6a5f344ba503daafe
Szymon Świędrych <szymek_sw@outlook.com>
2022-12-02 17:23:48 +0100

    Add score

--- Engine/CoinActor.py
+++ Engine/CoinActor.py
@@ -10,6 +10,8 @@ from Engine.SceneManager import SceneManager
 class CoinActor(CircleActor):
     def __init__(self, engine: MainEngine):
         super().__init__(engine, pygame.image.load("res/Images/Coin.png"), 8)
+        self._player_one_score = 0
+        self._player_two_score = 0
 
     def Start(self):
         super().Start()
@@ -19,8 +21,14 @@ class CoinActor(CircleActor):
 
         for another_actor in self.collided_actors_this_frame:
             if isinstance(another_actor, Player) or isinstance(another_actor, CirclePlayer):
+                if isinstance(another_actor, Player):
+                    self._player_one_score += 1
+                if isinstance(another_actor, CirclePlayer):
+                    self._player_two_score += 1
+
                 for actor in self._engine.get_actor_list():
                     if isinstance(actor, SceneManager):
                         actor.load_next_map()
                         self.collided_actors_this_frame.clear()
+                        print(f"PlayerOne {self._player_one_score} : {self._player_two_score} PlayerTwo")
                         break
